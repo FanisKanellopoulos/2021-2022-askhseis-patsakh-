e1172004a706f11b65d050ba8803ea3bb9a44cf5
FanisKanellopoulos <96315280+FanisKanellopoulos@users.noreply.github.com>
2022-02-06 21:40:24 +0200

    Update askhsh12.py

--- askhsh12.py
+++ askhsh12.py
@@ -7,57 +7,52 @@ data=res.text
 temp=json.loads(data)
 y=temp['round'] #saves last round
 
-#μετρητές για τα round με τις μεγαλήτερες ακολουθίες
-maxmemory_1=0
-maxmemory_0=0
-maxround_1="" 
-maxround_0=""
+sum=""
 
-print("this may take a few seconds...")
+print("Please wait, this may take a few seconds...")
 for i in range(100):
   url="https://drand.cloudflare.com/public/"
   y=str(y)
-  url=url+y #μετατροπή του url 
+  url=url+y #μετατροπή του url
+  #print (url) 
   
   res = requests.get(url)
   data=res.text
   temp=json.loads(data)
   x=temp['randomness']
   x=bin(int(x,16))
-  r=temp['round'] #saves every round temporary
+  x=str(x)
   
-  #μετρητές για τις ακολουθίες
-  counter_1=0
-  memory_0=0
-  counter_0=0
-  memory_1=0
-  for i in range(2,len(x)):
-    if x[i]=="1":
+  #print(x,"\n")
+  sum+=x[2:]      #ενώνει τα randomness
+  
+  #προετοιμασία για την μετατροπή του επόμενου url
+  y=int(y)
+  y=y-1   
+
+#print (sum)
+
+
+#μετρητές για τις ακολουθίες
+counter_1=0
+memory_0=0
+counter_0=0
+memory_1=0
+
+for i in range(len(sum)):
+    if sum[i]=="1":
       counter_1+=1
     else:
       if counter_1>memory_1:
         memory_1=counter_1
       counter_1=0
 
-    if x[i]=="0":  #είναι λίγο κουτός τρόπος γιατί πραγματοποιεί έλεγχο 2 φορές για το ίδιο πράγμα αλλά εκείνη την στιγμή δεν μπορούσα να σκεφτώ κάτι καλύτερο... 
+    if sum[i]=="0":  #είναι λίγο κουτός τρόπος γιατί πραγματοποιεί έλεγχο 2 φορές για το ίδιο πράγμα αλλά εκείνη την στιγμή δεν μπορούσα να σκεφτώ κάτι καλύτερο... 
       counter_0+=1
     else:
       if counter_0>memory_0:
         memory_0=counter_0
-      counter_0=0   
-  
-  #αποθηκεύει την μεγαλήτερη ακολουθία καθώς και το round από το οποίο βρέθηκε
-  if memory_1>maxmemory_1:
-    maxmemory_1=memory_1
-    maxround_1=r
-  
-  if memory_0>maxmemory_0:
-    maxmemory_0=memory_0
-    maxround_0=r
+      counter_0=0 
 
-  #προετοιμασία για την μετατροπή του επόμενου url
-  y=int(y)
-  y=y-1   
-
-print("Το μήκος της μεγαλύτερης ακολουθίας με συνεχόμενα μηδενικά είναι:",maxmemory_0,"και εμφανίστηκε στο round:",maxround_0)    
-print("Το μήκος της μεγαλύτερης ακολουθίας με συνεχόμενους άσσους είναι:",maxmemory_1,"και εμφανίστηκε στο round:",maxround_1)
+print("Το μήκος της μεγαλύτερης ακολουθίας με συνεχόμενα μηδενικά είναι:",memory_0)
+print("Το μήκος της μεγαλύτερης ακολουθίας με συνεχόμενους άσσους είναι:",memory_1)
